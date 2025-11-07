9e363df241c0397007314a0a5302c957c9a4715b
Peter <85247098+RedFox86@users.noreply.github.com>
2025-11-07 16:16:32 -0500

    Remove documenation

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -34,36 +34,47 @@ import java.util.function.Supplier;
 
 @Logged
 public class Robot extends TimedRobot {
-  private Command autonomousCommand;
-  @Logged private final CommandScheduler scheduler = CommandScheduler.getInstance();
-
-  // The robot's subsystems
   private final DriveSubsystem drive;
   private final Vision vision;
+  private final LiveIssue liveIssue;
 
-  private final int turnMultiplier = 1;
+  @NotLogged private final CommandXboxController operatorController;
+  @NotLogged private final CommandJoystick rStick;
+  @NotLogged private final CommandJoystick lStick;
 
-  // Driver and operator controls
-  @NotLogged private final CommandXboxController operatorController; // NOPMD
-  @NotLogged private final CommandJoystick rStick; // NOPMD
-  @NotLogged private final CommandJoystick lStick; // NOPMD
+  private double globalTurnSpeedMultiplier = 1;
+  private double globalDriveSpeedMultiplier = 1;
 
-  @SuppressWarnings("FieldCanBeLocal")
-  private final SendableChooser<Supplier<Command>> odometryTestChooser = new SendableChooser<>();
-
-  private final SendableChooser<Supplier<Command>> autoChooser = new SendableChooser<>();
+  private final SendableChooser<Supplier<Command>> odometryTestChooser;
+  private final SendableChooser<Supplier<Command>> autoChooser;
 
   public Robot() {
-    // Initialize our subsystems. If our program is running in simulation mode (either from the
-    // simulate command in vscode or from running in unit tests), then we use the simulation IO
-    // layers. Otherwise, the IO layers that interact with real hardware are used.
     if (Robot.isSimulation()) {
       drive = new DriveSubsystem(new SimSwerveIO());
     } else {
       drive = new DriveSubsystem(new MAXSwerveIO());
     }
     vision = new Vision();
+    liveIssue = LiveIssue.INSTANCE;
 
+    operatorController = new CommandXboxController(Constants.OIConstants.driverControllerPort);
+    rStick = new CommandJoystick(Constants.OIConstants.rightJoystickPort);
+    lStick = new CommandJoystick(Constants.OIConstants.leftJoystickPort);
+
+    odometryTestChooser = new SendableChooser<>();
+    autoChooser = new SendableChooser<>();
+
+    configureDriveDistanceChooser();
+    configureAutonomous();
+    configureButtonBindings();
+    configureDefaultCommands();
+    startLogging();
+
+    DriverStation.silenceJoystickConnectionWarning(true);
+  }
+
+  // region | CONFIGURE METHODS |
+  private void configureDriveDistanceChooser() {
     odometryTestChooser.setDefaultOption("Do Nothing", Commands::none);
     odometryTestChooser.addOption("Drive 1 Foot", () -> drive.driveDistance(Feet.of(1)));
     odometryTestChooser.addOption("Drive 2 Feet", () -> drive.driveDistance(Feet.of(2)));
@@ -73,30 +84,25 @@ public class Robot extends TimedRobot {
     odometryTestChooser.addOption("Drive 13 Feet", () -> drive.driveDistance(Feet.of(13)));
     odometryTestChooser.addOption("Drive 21 Feet", () -> drive.driveDistance(Feet.of(21)));
 
+    Shuffleboard.getTab("Test").add("Drive Distance Selection", odometryTestChooser);
+  }
+
+  private void configureButtonBindings() {
+    operatorController.a().whileTrue(drive.driveToRobotRelativePose(vision.getLastRealValue().toPose2d()));
+    operatorController.x().whileTrue(drive.driveDistance(Feet.of(3)));
+  }
+
+  private void configureDefaultCommands() {
+    drive.setDefaultCommand(driveWithFlightSticks());
+  }
+
+  private void configureAutonomous() {
     autoChooser.setDefaultOption("Do Nothing", Commands::none);
     autoChooser.addOption("10s Leave", drive::autoLeaveArea);
-
-    Shuffleboard.getTab("Test").add("Drive Distance Selection", odometryTestChooser);
     SmartDashboard.putData("Auto Chooser", autoChooser);
+  }
 
-    operatorController = new CommandXboxController(Constants.OIConstants.driverControllerPort);
-    rStick = new CommandJoystick(Constants.OIConstants.leftJoystickPort);
-    lStick = new CommandJoystick(Constants.OIConstants.rightJoystickPort);
-
-    // Configure the button bindings and automatic bindings
-
-    // Configure default commands
-    /*
-     * Use driveWithFlightSticks() to use flight stick driving
-     * Use driveWithXbox() to drive solely with the xbox controller
-     * Note: Right joystick drives, left joystick turns for both
-     * xbox and flight sticks. Refer to Constants.java (OIConstants)
-     * for the correct Driver Station inputs.
-     */
-    drive.setDefaultCommand(driveFastWithFlightSticks());
-
-    // Start data logging
-
+  private void startLogging() {
     SignalLogger.start();
 
     Epilogue.configure(
@@ -107,39 +113,19 @@ public class Robot extends TimedRobot {
                     new NTEpilogueBackend(NetworkTableInstance.getDefault())));
 
     DriverStation.startDataLog(DataLogManager.getLog(), true);
-
-    // Disable joystick warnings by default
-    DriverStation.silenceJoystickConnectionWarning(true);
   }
 
-  @SuppressWarnings("unused")
-  private Command driveWithXbox() {
-    //noinspection SuspiciousNameCombination
-    return drive.driveFastWithJoysticks(
-        operatorController::getLeftY, operatorController::getLeftX, operatorController::getRightX);
+  // endregion
+  // region | COMMANDS |
+  private Command driveWithFlightSticks() {
+    return drive.driveXYTheta(
+        () -> rStick.getY() * globalDriveSpeedMultiplier,
+        () -> rStick.getX() * globalDriveSpeedMultiplier,
+        () -> lStick.getTwist() * globalTurnSpeedMultiplier);
   }
 
-  private Command driveFastWithFlightSticks() {
-    //noinspection SuspiciousNameCombination
-    return drive.driveFastWithJoysticks(
-        lStick::getY, lStick::getX, () -> rStick.getTwist() * turnMultiplier);
-  }
-
-  @SuppressWarnings("unused")
-  private Command driveSlowWithFlightSticks() {
-    //noinspection SuspiciousNameCombination
-    return drive.driveSlowWithJoysticks(lStick::getY, lStick::getX, rStick::getTwist);
-  }
-
-  /**
-   * Gets the command to run in autonomous based on user selection in a dashboard.
-   *
-   * @return the command to run in autonomous
-   */
-  public Command getAutonomousCommand() {
-    return autoChooser.getSelected().get();
-  }
-
+  // endregion
+  // region | INFO METHODS |
   @Logged(name = "Battery Voltage")
   public double getBatteryVoltage() {
     return RobotController.getBatteryVoltage();
@@ -150,31 +136,18 @@ public class Robot extends TimedRobot {
     return RobotController.getInputCurrent();
   }
 
-  /**
-   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
-   * that you want ran during disabled, autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
-   * SmartDashboard integrated updating.
-   */
+  // endregion
+  // region | ROBOT OVERRIDES |
+  // Runs after specific periodic functions
   @Override
   public void robotPeriodic() {
     if (DriverStation.isFMSAttached()) {
-      // Enable joystick warnings on the field
       DriverStation.silenceJoystickConnectionWarning(false);
     }
 
     vision.update();
-
     Epilogue.update(this);
-
-    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
-    // commands, running already-scheduled commands, removing finished or interrupted commands,
-    // and running subsystem periodic() methods.  This must be called from the robot's periodic
-    // block in order for anything in the Command-based framework to work.
-
     CommandScheduler.getInstance().run();
-
     SmartDashboard.putNumber("Match Time", DriverStation.getMatchTime());
   }
 
@@ -183,30 +156,19 @@ public class Robot extends TimedRobot {
     SimulationContext.getDefault().update(getPeriod());
   }
 
-  /** This function is called once each time the robot enters Disabled mode. */
-  @Override
-  public void disabledInit() {}
-
-  @Override
-  public void disabledPeriodic() {}
-
-  /** This autonomous runs the autonomous command selected by the autoChooser. */
   @Override
   public void autonomousInit() {
-    autonomousCommand = getAutonomousCommand();
+    Command autonomousCommand = autoChooser.getSelected().get();
 
-    // schedule the autonomous command (example)
     if (autonomousCommand != null) {
       autonomousCommand.schedule();
     }
   }
 
   @Override
-  public void autonomousPeriodic() {}
-
-  @Override
-  public void teleopPeriodic() {}
-
-  @Override
-  public void testPeriodic() {}
+  public void teleopPeriodic() {
+    globalTurnSpeedMultiplier = 1-(lStick.getThrottle()+1)/2;
+    globalDriveSpeedMultiplier = 1-(rStick.getThrottle()+1)/2;
+  }
+  // endregion
 }
