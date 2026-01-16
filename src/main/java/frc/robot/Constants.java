279c9812748bf046f2b3098377c092d57db2f774
Peter <85247098+RedFox86@users.noreply.github.com>
2026-01-15 20:51:08 -0500

    Add wrist intake constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,8 +1,11 @@
 package frc.robot;
 
-import static edu.wpi.first.units.Units.Volts;
-
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.AngularAcceleration;
+import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.units.measure.Voltage;
+
+import static edu.wpi.first.units.Units.*;
 
 public class Constants {
   public static class InputConstants {
@@ -14,7 +17,20 @@ public class Constants {
     public static final int WRIST_CAN_ID = 10;
   }
   public static class IntakeConstants {
+    public static final double KP = 2;
+    public static final double KI = 0;
+    public static final double KD = 0.2;
+    public static final double KS = 0.014847;
+    public static final double KG = 0.3732;
+    public static final double KV = 1.1878;
+    public static final double KA = 0.0024773;
+
     public static final Voltage INTAKE_VOLTAGE = Volts.of(5);
     public static final Voltage OUTTAKE_VOLTAGE = Volts.of(-5);
+    public static final AngularVelocity MAX_WRIST_SPEED = RadiansPerSecond.of(2);
+    public static final AngularAcceleration MAX_WRIST_ACCELERATION = RadiansPerSecondPerSecond.of(1);
+    public static final Angle WRIST_TOLERANCE = Degrees.of(5);
+    public static final Angle WRIST_STOW_ANGLE = Radians.of(Math.PI);
+    public static final Angle WRIST_EXTEND_ANGLE = Degrees.of(30);
   }
 }
