a5e6c42c626a7608c54ac319445ab46824a66514
Peter <85247098+PeterLangendorf@users.noreply.github.com>
2026-01-21 20:25:12 -0500

    Rename ShooterIO methods

--- src/main/java/frc/robot/subsystems/shooter/ShooterIO.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterIO.java
@@ -6,13 +6,30 @@ import edu.wpi.first.units.measure.Current;
 
 @Logged
 public interface ShooterIO {
-
+	/**
+	 * Stop the flywheel of the shooter.
+	 */
 	void stop();
 
-	void setSpeed(AngularVelocity angularVelocity);
+	/**
+	 * Set the desired velocity of the flywheel, to be reached via PID and FeedForward within the implementations.
+	 *
+	 * @param angularVelocity
+	 *            The speed that the flywheel should go to.
+	 */
+	void setDesiredVelocity(AngularVelocity angularVelocity);
 
-	AngularVelocity getSpeed();
+	/**
+	 * Returns the current velocity that the flywheel in the shooter is running at.
+	 *
+	 * @return The flywheel's velocity.
+	 */
+	AngularVelocity getVelocity();
 
+	/**
+	 * Returns the current draw that the mechanism is currently consuming.
+	 *
+	 * @return The mechanism's current draw.
+	 */
 	Current getCurrentDraw();
-
 }
