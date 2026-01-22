928520d517b1823ef3872f7cef1f7e99d840e3cb
Peter <85247098+PeterLangendorf@users.noreply.github.com>
2026-01-21 20:24:58 -0500

    Format Shooter.java

--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -1,7 +1,6 @@
 package frc.robot.subsystems.shooter;
 
-import static edu.wpi.first.units.Units.*;
-import static frc.robot.Constants.IntakeConstants.*;
+import static edu.wpi.first.units.Units.RPM;
 
 import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -9,12 +8,11 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import java.util.function.Supplier;
 
 import edu.wpi.first.units.measure.AngularVelocity;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 @Logged
 public class Shooter extends SubsystemBase {
 	private final ShooterIO io;
+
 	public Shooter(ShooterIO io) {
 		this.io = io;
 	}
