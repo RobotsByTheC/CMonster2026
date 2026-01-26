a94a83e4e7511269b5ab936946f6ad1ea5fe90f3
Peter <85247098+PeterLangendorf@users.noreply.github.com>
2026-01-25 23:08:32 -0500

    Redesign interpolator to not convert units

--- src/main/java/frc/robot/subsystems/data/InterpolatingShooterTreeMap.java
+++ src/main/java/frc/robot/subsystems/data/InterpolatingShooterTreeMap.java
@@ -5,6 +5,7 @@ import edu.wpi.first.math.Pair;
 import edu.wpi.first.math.interpolation.InterpolatingTreeMap;
 import edu.wpi.first.math.interpolation.Interpolator;
 import edu.wpi.first.math.interpolation.InverseInterpolator;
+import edu.wpi.first.units.Measure;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.units.measure.Distance;
@@ -15,11 +16,9 @@ import static edu.wpi.first.units.Units.Radians;
 import static edu.wpi.first.units.Units.RadiansPerSecond;
 
 public class InterpolatingShooterTreeMap extends InterpolatingTreeMap<Distance, Pair<AngularVelocity, Angle>> {
-  private static final Interpolator<Pair<AngularVelocity, Angle>> INTERPOLATOR = (startValue, endValue, t) -> new Pair<>(
-      RadiansPerSecond.of(MathUtil.interpolate(startValue.getFirst().in(RadiansPerSecond), endValue.getFirst().in(RadiansPerSecond), t)),
-      Radians.of(MathUtil.interpolate(startValue.getSecond().in(Radians), endValue.getSecond().in(Radians), t)));
+
 
   public InterpolatingShooterTreeMap() {
-    super(null, INTERPOLATOR, Comparator.naturalOrder());
+    super(null, (startValue, endValue, t) -> new Pair<>(startValue.getFirst().plus(endValue.getFirst().minus(startValue.getFirst())).times(MathUtil.clamp(t, 0, 1)), startValue.getSecond().plus(endValue.getSecond().minus(startValue.getSecond())).times(MathUtil.clamp(t, 0, 1))), Comparator.naturalOrder());
   }
 }
