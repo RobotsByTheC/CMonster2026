01d42383389d450ce59eda063f27c81b65f5527e
Peter <85247098+PeterLangendorf@users.noreply.github.com>
2026-01-26 17:35:44 -0500

    Try to live update distance in command

--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -7,6 +7,7 @@ import edu.wpi.first.math.Pair;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import java.util.function.Supplier;
 
@@ -16,43 +17,23 @@ import frc.robot.Robot;
 @Logged
 public class Shooter extends SubsystemBase {
 	private final ShooterIO io;
-	public Distance lastDistanceToTarget = Meters.zero();
+  private Distance lastDistanceToTarget = Meters.zero();
 
 	public Shooter(ShooterIO io) {
 		this.io = io;
 	}
 
-	private void updateTarget(Distance distanceToTarget) {
-		if (distanceToTarget.isNear(lastDistanceToTarget, Inches.of(2))) return;
-		io.setFlywheelVelocity(LookupTable.SPEED_TABLE.get(distanceToTarget));
-		io.setHoodAngle(LookupTable.ANGLE_TABLE.get(distanceToTarget));
-		lastDistanceToTarget = distanceToTarget;
-	}
-
-  private void simUpdateTarget(Distance distanceToTarget) {
-    io.setFlywheelVelocity(LookupTable.SPEED_TABLE.get(distanceToTarget));
-    io.setHoodAngle(LookupTable.ANGLE_TABLE.get(distanceToTarget));
-    System.out.println(Math.random() + "| " + distanceToTarget + LookupTable.SPEED_TABLE.get(distanceToTarget));
-  }
-
-	public Command f_shootAtTarget(Supplier<Distance> distance) {
-		return run(() -> {
-      if (Robot.isSimulation()) {
-        simUpdateTarget(distance.get());
-      } else {
-        updateTarget(distance.get());
-      }
-    });
-	}
-
-  public Command f_holdPos() {
+  public Command f_shootDistance(Supplier<Distance> distance) {
     return run(() -> {
-      io.setFlywheelVelocity(RPM.zero());
-      io.setHoodAngle(Degrees.zero());
+      if (!distance.get().isNear(lastDistanceToTarget, Inches.of(0.01)) || lastDistanceToTarget.isEquivalent(Meters.zero())) {
+        io.setFlywheelVelocity(() -> LookupTable.SPEED_TABLE.get(distance.get()));
+        io.setHoodAngle(() -> LookupTable.ANGLE_TABLE.get(distance.get()));
+        lastDistanceToTarget = distance.get();
+      }
     });
   }
 
 	public Command o_stop() {
-		return runOnce(io::stopFlywheel).andThen(run(io::stopHood));
+		return runOnce(io::stopFlywheel).andThen(runOnce(io::stopHood)).andThen(Commands.runOnce(() -> lastDistanceToTarget = Meters.zero()));
 	}
 }
