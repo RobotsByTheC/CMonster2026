46bc5ef7667ef6684d2cda1cd130f065203be282
Peter <85247098+PeterLangendorf@users.noreply.github.com>
2026-01-25 19:34:59 -0500

    Update ShooterIO with new components

--- src/main/java/frc/robot/subsystems/shooter/ShooterIO.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterIO.java
@@ -1,35 +1,41 @@
 package frc.robot.subsystems.shooter;
 
 import edu.wpi.first.epilogue.Logged;
+import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.units.measure.Voltage;
 
 @Logged
 public interface ShooterIO {
 	/**
 	 * Stop the flywheel of the shooter.
 	 */
-	void stop();
+	void stopFlywheel();
+
+  void stopIntermediary();
+
+  void stopHood();
+
+  /**
+   * Returns the current velocity that the flywheel in the shooter is running at.
+   * @return The flywheel's velocity.
+   */
+  AngularVelocity getFlywheelVelocity();
+
+  /**
+   * Returns the current draw that the mechanism is currently consuming.
+   * @return The mechanism's current draw.
+   */
+  Current getCurrentDraw();
 
 	/**
 	 * Set the desired velocity of the flywheel, to be reached via PID and FeedForward within the implementations.
-	 *
-	 * @param angularVelocity
-	 *            The speed that the flywheel should go to.
+	 * @param velocity The speed that the flywheel should go to.
 	 */
-	void setDesiredVelocity(AngularVelocity angularVelocity);
+	void setFlywheelVelocity(AngularVelocity velocity);
 
-	/**
-	 * Returns the current velocity that the flywheel in the shooter is running at.
-	 *
-	 * @return The flywheel's velocity.
-	 */
-	AngularVelocity getVelocity();
+  void setIntermediaryVoltage(Voltage voltage);
 
-	/**
-	 * Returns the current draw that the mechanism is currently consuming.
-	 *
-	 * @return The mechanism's current draw.
-	 */
-	Current getCurrentDraw();
+  void setHoodVoltage(Voltage voltage);
 }
