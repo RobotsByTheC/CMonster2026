249adfe02944cffb1481b629a7e2248fe15b468a
Peter <85247098+PeterLangendorf@users.noreply.github.com>
2026-01-25 23:56:43 -0500

    Finish the fieldless constructor

--- src/main/java/frc/robot/subsystems/data/InterpolatingShooterTreeMap.java
+++ src/main/java/frc/robot/subsystems/data/InterpolatingShooterTreeMap.java
@@ -3,9 +3,6 @@ package frc.robot.subsystems.data;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.Pair;
 import edu.wpi.first.math.interpolation.InterpolatingTreeMap;
-import edu.wpi.first.math.interpolation.Interpolator;
-import edu.wpi.first.math.interpolation.InverseInterpolator;
-import edu.wpi.first.units.Measure;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.units.measure.Distance;
@@ -15,13 +12,16 @@ import java.util.Comparator;
 import static edu.wpi.first.units.Units.*;
 
 public class InterpolatingShooterTreeMap extends InterpolatingTreeMap<Distance, Pair<AngularVelocity, Angle>> {
-  private static final InverseInterpolator<Distance> inverseInterpolator = (startValue, endValue, q) -> {
-    Distance range = endValue.minus(startValue);
-    Distance query = q.minus(startValue);
-    return (range.lte(Meters.zero()) || query.lte(Meters.zero())) ? 0 : query.div(range).baseUnitMagnitude();
-  };
-
-  public InterpolatingShooterTreeMap() {
-    super(inverseInterpolator, (startValue, endValue, t) -> new Pair<>(startValue.getFirst().plus(endValue.getFirst().minus(startValue.getFirst())).times(MathUtil.clamp(t, 0, 1)), startValue.getSecond().plus(endValue.getSecond().minus(startValue.getSecond())).times(MathUtil.clamp(t, 0, 1))), Comparator.naturalOrder());
-  }
+	public InterpolatingShooterTreeMap() {
+		super((startValue, endValue, q) -> {
+			Distance range = endValue.minus(startValue);
+			Distance query = q.minus(startValue);
+			return (range.lte(Meters.zero()) || query.lte(Meters.zero())) ? 0 : query.div(range).baseUnitMagnitude();
+		}, (startValue, endValue, t) -> new Pair<>(
+				startValue.getFirst().plus(endValue.getFirst().minus(startValue.getFirst()))
+						.times(MathUtil.clamp(t, 0, 1)),
+				startValue.getSecond().plus(endValue.getSecond().minus(startValue.getSecond()))
+						.times(MathUtil.clamp(t, 0, 1))),
+				Comparator.naturalOrder());
+	}
 }
