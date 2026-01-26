c962533da8891edd460b72de865f02a3e9da3273
Peter <85247098+PeterLangendorf@users.noreply.github.com>
2026-01-25 23:55:23 -0500

    Translate inverse interpolator into Distance

--- src/main/java/frc/robot/subsystems/data/InterpolatingShooterTreeMap.java
+++ src/main/java/frc/robot/subsystems/data/InterpolatingShooterTreeMap.java
@@ -12,17 +12,16 @@ import edu.wpi.first.units.measure.Distance;
 
 import java.util.Comparator;
 
-import static edu.wpi.first.units.Units.Radians;
-import static edu.wpi.first.units.Units.RadiansPerSecond;
+import static edu.wpi.first.units.Units.*;
 
 public class InterpolatingShooterTreeMap extends InterpolatingTreeMap<Distance, Pair<AngularVelocity, Angle>> {
-InverseInterpolator<Pair<AngularVelocity, Angle>> inverseInterpolator = (startValue, endValue, q) -> {
-  AngularVelocity range = endValue.getFirst().minus(startValue.getFirst());
-  AngularVelocity query = q.getFirst().minus(startValue.getFirst());
-  return (range.lte(RadiansPerSecond.zero()) || query.lte(RadiansPerSecond.zero())) ? 0 : query.div(range).baseUnitMagnitude();
-};
+  private static final InverseInterpolator<Distance> inverseInterpolator = (startValue, endValue, q) -> {
+    Distance range = endValue.minus(startValue);
+    Distance query = q.minus(startValue);
+    return (range.lte(Meters.zero()) || query.lte(Meters.zero())) ? 0 : query.div(range).baseUnitMagnitude();
+  };
 
   public InterpolatingShooterTreeMap() {
-    super(null, (startValue, endValue, t) -> new Pair<>(startValue.getFirst().plus(endValue.getFirst().minus(startValue.getFirst())).times(MathUtil.clamp(t, 0, 1)), startValue.getSecond().plus(endValue.getSecond().minus(startValue.getSecond())).times(MathUtil.clamp(t, 0, 1))), Comparator.naturalOrder());
+    super(inverseInterpolator, (startValue, endValue, t) -> new Pair<>(startValue.getFirst().plus(endValue.getFirst().minus(startValue.getFirst())).times(MathUtil.clamp(t, 0, 1)), startValue.getSecond().plus(endValue.getSecond().minus(startValue.getSecond())).times(MathUtil.clamp(t, 0, 1))), Comparator.naturalOrder());
   }
 }
