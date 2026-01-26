b4d9c88466177881a46e3666064cc7c18c15313d
Peter <85247098+PeterLangendorf@users.noreply.github.com>
2026-01-26 14:27:02 -0500

    Update Shooter to match

--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -11,37 +11,48 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import java.util.function.Supplier;
 
 import edu.wpi.first.units.measure.AngularVelocity;
+import frc.robot.Robot;
 
 @Logged
 public class Shooter extends SubsystemBase {
 	private final ShooterIO io;
-	public double lastDistanceToTarget = 0;
+	public Distance lastDistanceToTarget = Meters.zero();
 
 	public Shooter(ShooterIO io) {
 		this.io = io;
 	}
 
 	private void updateTarget(Distance distanceToTarget) {
-		int newDistance = (int) (Math.round(distanceToTarget.in(Meters) * 10) / 10d);
-		if (newDistance == lastDistanceToTarget)
-			return;
-		Pair<AngularVelocity, Angle> lookup = LookupTable.SHOOTER_LOOKUP_TABLE.get(Meters.of(newDistance));
-		io.setFlywheelVelocity(lookup.getFirst());
-		io.setHoodAngle(lookup.getSecond());
-		lastDistanceToTarget = newDistance;
+		if (distanceToTarget.isNear(lastDistanceToTarget, Inches.of(2))) return;
+		io.setFlywheelVelocity(LookupTable.SPEED_TABLE.get(distanceToTarget));
+		io.setHoodAngle(LookupTable.ANGLE_TABLE.get(distanceToTarget));
+		lastDistanceToTarget = distanceToTarget;
 	}
+
+  private void simUpdateTarget(Distance distanceToTarget) {
+    io.setFlywheelVelocity(LookupTable.SPEED_TABLE.get(distanceToTarget));
+    io.setHoodAngle(LookupTable.ANGLE_TABLE.get(distanceToTarget));
+    System.out.println(Math.random() + "| " + distanceToTarget + LookupTable.SPEED_TABLE.get(distanceToTarget));
+  }
 
 	public Command f_shootAtTarget(Supplier<Distance> distance) {
-		return run(() -> updateTarget(distance.get()));
+		return run(() -> {
+      if (Robot.isSimulation()) {
+        simUpdateTarget(distance.get());
+      } else {
+        updateTarget(distance.get());
+      }
+    });
 	}
 
-	public Command stop() {
   public Command f_holdPos() {
     return run(() -> {
       io.setFlywheelVelocity(RPM.zero());
       io.setHoodAngle(Degrees.zero());
     });
   }
+
+	public Command o_stop() {
 		return runOnce(io::stopFlywheel).andThen(run(io::stopHood));
 	}
 }
