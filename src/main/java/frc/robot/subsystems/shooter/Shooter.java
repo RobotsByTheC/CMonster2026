8ca0cdaa7984ea142974cf4d67a9ab4cf61a5d73
Peter <85247098+PeterLangendorf@users.noreply.github.com>
2026-01-26 19:48:33 -0500

    Transition back to subclass style

--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -8,6 +8,7 @@ import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import java.util.function.Supplier;
 
@@ -19,21 +20,22 @@ public class Shooter extends SubsystemBase {
 	private final ShooterIO io;
   private Distance lastDistanceToTarget = Meters.zero();
 
+  class Flywheel extends SubsystemBase {
+    
+    public Command stop() {
+      return runOnce(io::stopFlywheel);
+    }
+  }
+
+  class Hood extends SubsystemBase {
+    public Command stop() {
+      return runOnce(io::stopHood);
+    }
+  }
+
 	public Shooter(ShooterIO io) {
 		this.io = io;
 	}
 
-  public Command f_shootDistance(Supplier<Distance> distance) {
-    return run(() -> {
-      if (!distance.get().isNear(lastDistanceToTarget, Inches.of(0.01)) || lastDistanceToTarget.isEquivalent(Meters.zero())) {
-        io.setFlywheelVelocity(() -> LookupTable.SPEED_TABLE.get(distance.get()));
-        io.setHoodAngle(() -> LookupTable.ANGLE_TABLE.get(distance.get()));
-        lastDistanceToTarget = distance.get();
-      }
-    });
-  }
 
-	public Command o_stop() {
-		return runOnce(io::stopFlywheel).andThen(runOnce(io::stopHood)).andThen(Commands.runOnce(() -> lastDistanceToTarget = Meters.zero()));
-	}
 }
