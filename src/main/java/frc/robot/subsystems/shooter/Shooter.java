840591e2761d32c38ed4e2225f6ecae0a9240b4c
Peter <85247098+PeterLangendorf@users.noreply.github.com>
2026-01-26 20:09:52 -0500

    Return to single-class system

--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -6,10 +6,13 @@ import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.math.Pair;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.units.measure.MutDistance;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import java.sql.SQLOutput;
 import java.util.function.Supplier;
 
 import edu.wpi.first.units.measure.AngularVelocity;
@@ -18,29 +21,10 @@ import frc.robot.Robot;
 @Logged
 public class Shooter extends SubsystemBase {
 	private final ShooterIO io;
-  private Distance lastDistanceToTarget = Meters.zero();
 
-  class Flywheel extends SubsystemBase {
-    public Command setTargetVelocity(AngularVelocity velocity) {
-      return runOnce(() -> io.setFlywheelVelocity(velocity));
-    }
-    public Command stop() {
-      return runOnce(io::stopFlywheel);
-    }
-  }
-
-  class Hood extends SubsystemBase {
-    public Command setTargetAngle(Angle angle) {
-      return runOnce(() -> io.setHoodAngle(angle));
-    }
-    public Command stop() {
-      return runOnce(io::stopHood);
-    }
-  }
+  private MutDistance lastDistanceToTarget = Meters.mutable(0);
 
 	public Shooter(ShooterIO io) {
 		this.io = io;
 	}
-
-
 }
