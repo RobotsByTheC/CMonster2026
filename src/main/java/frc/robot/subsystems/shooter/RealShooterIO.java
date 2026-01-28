23471ab17a4fecb925bb6e5cce9a4ac5fc57a65a
Peter <85247098+PeterLangendorf@users.noreply.github.com>
2026-01-28 14:07:04 -0500

    Remove unused imports

--- src/main/java/frc/robot/subsystems/shooter/RealShooterIO.java
+++ src/main/java/frc/robot/subsystems/shooter/RealShooterIO.java
@@ -17,9 +17,6 @@ import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.units.measure.Current;
-import edu.wpi.first.units.measure.Voltage;
-
-import java.util.function.Supplier;
 
 @Logged
 public class RealShooterIO implements ShooterIO {
