fc5bdd3f2cadbccc3653122622288843e7384b83
Peter <85247098+PeterLangendorf@users.noreply.github.com>
2026-01-28 14:06:50 -0500

    Remove unused imports

--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -3,20 +3,12 @@ package frc.robot.subsystems.shooter;
 import static edu.wpi.first.units.Units.*;
 
 import edu.wpi.first.epilogue.Logged;
-import edu.wpi.first.math.Pair;
-import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.units.measure.MutDistance;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-import java.sql.SQLOutput;
 import java.util.function.Supplier;
-
-import edu.wpi.first.units.measure.AngularVelocity;
-import frc.robot.Robot;
 
 @Logged
 public class Shooter extends SubsystemBase {
