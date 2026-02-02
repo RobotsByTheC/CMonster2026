8092db09ddb2fd1e4429c4ba9f62578bc5e2ebfe
Peter <85247098+PeterLangendorf@users.noreply.github.com>
2026-02-01 20:48:29 -0500

    Implement RealIO

---
+++ src/main/java/frc/robot/subsystems/shooter/hood/RealHoodIO.java
@@ -0,0 +1,42 @@
+package frc.robot.subsystems.shooter.hood;
+
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkLimitSwitch;
+import com.revrobotics.spark.SparkLowLevel;
+import com.revrobotics.spark.SparkMax;
+import edu.wpi.first.epilogue.Logged;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Voltage;
+
+import static edu.wpi.first.units.Units.RPM;
+import static edu.wpi.first.units.Units.Rotations;
+import static frc.robot.Constants.CANConstants.HOOD_CAN_ID;
+
+@Logged
+public class RealHoodIO implements HoodIO {
+	private final SparkMax spark;
+	private final RelativeEncoder encoder;
+	private final SparkLimitSwitch limitSwitch;
+
+	public RealHoodIO() {
+		spark = new SparkMax(HOOD_CAN_ID, SparkLowLevel.MotorType.kBrushless);
+		encoder = spark.getEncoder();
+		limitSwitch = spark.getForwardLimitSwitch();
+	}
+
+	@Override
+	public Angle getAngle() {
+		return Rotations.of(encoder.getPosition());
+	}
+
+	@Override
+	public AngularVelocity getVelocity() {
+		return RPM.of(encoder.getVelocity());
+	}
+
+	@Override
+	public void setVoltage(Voltage voltage) {
+		spark.setVoltage(voltage);
+	}
+}
