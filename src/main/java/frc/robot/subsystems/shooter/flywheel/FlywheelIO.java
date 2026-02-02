6c9f9f31dcff27fb99071e2f0d93f502b909138a
Peter <85247098+PeterLangendorf@users.noreply.github.com>
2026-02-01 20:47:51 -0500

    Add IO to flywheel

---
+++ src/main/java/frc/robot/subsystems/shooter/flywheel/FlywheelIO.java
@@ -0,0 +1,35 @@
+package frc.robot.subsystems.shooter.flywheel;
+
+import edu.wpi.first.epilogue.Logged;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Current;
+
+@Logged
+public interface FlywheelIO {
+	/**
+	 * Stop the flywheel of the shooter.
+	 */
+	void stop();
+
+	/**
+	 * Returns the current velocity that the flywheel in the shooter is running at.
+	 *
+	 * @return The flywheel's velocity.
+	 */
+	AngularVelocity getVelocity();
+
+	/**
+	 * Returns the current draw that the mechanism is currently consuming.
+	 *
+	 * @return The mechanism's current draw.
+	 */
+	Current getCurrentDraw();
+
+	/**
+	 * Set the desired velocity of the flywheel, to be reached via PID and FeedForward within the implementations.
+	 *
+	 * @param velocity
+	 *            The speed that the flywheel should go to.
+	 */
+	void setVelocity(AngularVelocity velocity);
+}
