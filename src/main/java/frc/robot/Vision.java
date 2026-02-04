fed971ea1f8a5e6fb9321e3b091a0153be2d61a6
Peter <85247098+PeterLangendorf@users.noreply.github.com>
2026-02-04 18:43:54 -0500

    Add logged tag

--- src/main/java/frc/robot/Vision.java
+++ src/main/java/frc/robot/Vision.java
@@ -1,8 +1,10 @@
 package frc.robot;
 
+import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.math.geometry.Pose2d;
 import org.photonvision.PhotonCamera;
 
+@Logged
 public class Vision {
 	private final PhotonCamera rightCamera = new PhotonCamera("OV9281-1");
 	private final PhotonCamera leftCamera = new PhotonCamera("OV9281-2");
