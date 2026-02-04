3d4d133c03fc9bcae9c5b455858e3be37deb3fc2
Peter <85247098+PeterLangendorf@users.noreply.github.com>
2026-02-04 18:43:46 -0500

    Add photon cameras

--- src/main/java/frc/robot/Vision.java
+++ src/main/java/frc/robot/Vision.java
@@ -1,8 +1,11 @@
 package frc.robot;
 
 import edu.wpi.first.math.geometry.Pose2d;
+import org.photonvision.PhotonCamera;
 
 public class Vision {
+	private final PhotonCamera rightCamera = new PhotonCamera("OV9281-1");
+	private final PhotonCamera leftCamera = new PhotonCamera("OV9281-2");
 	public Pose2d getTarget() {
 		return Pose2d.kZero;
 	}
