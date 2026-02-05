f68274ccda083cf4fa6f035f0951b2e5a2084692
Peter <85247098+PeterLangendorf@users.noreply.github.com>
2026-02-05 15:14:58 -0500

    Add camera offset variables

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,7 +1,9 @@
 package frc.robot;
 
-import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.units.measure.Angle;
@@ -126,7 +128,7 @@ public class Constants {
 	}
 
 	public static class VisionConstants {
-		public static final Pose3d LEFT_CAMERA_OFFSET = new Pose3d(0, 0, 0, Rotation3d.kZero);
-		public static final Pose3d RIGHT_CAMERA_OFFSET = new Pose3d(0, 0, 0, Rotation3d.kZero);
+		public static final Transform3d LEFT_CAMERA_OFFSET = new Transform3d(0, 0, 0, Rotation3d.kZero);
+		public static final Transform3d RIGHT_CAMERA_OFFSET = new Transform3d(0, 0, 0, Rotation3d.kZero);
 	}
 }
