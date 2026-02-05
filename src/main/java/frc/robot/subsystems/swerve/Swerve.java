20d3fbc8d7be16eb48f3c9461c2cea6a186302d1
Peter <85247098+PeterLangendorf@users.noreply.github.com>
2026-02-05 15:16:26 -0500

    Create pose estimation and migrate subsystems to use it

--- src/main/java/frc/robot/subsystems/swerve/Swerve.java
+++ src/main/java/frc/robot/subsystems/swerve/Swerve.java
@@ -1,11 +1,13 @@
 package frc.robot.subsystems.swerve;
 
+import static edu.wpi.first.units.Units.Radians;
 import static edu.wpi.first.units.Units.RadiansPerSecond;
 import static edu.wpi.first.units.Units.RadiansPerSecondPerSecond;
 import static frc.robot.Constants.SwerveConstants.DriveConstants;
 import static frc.robot.Constants.SwerveConstants.TurnConstants;
 
 import edu.wpi.first.epilogue.Logged;
+import edu.wpi.first.epilogue.NotLogged;
 import edu.wpi.first.math.controller.HolonomicDriveController;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.ProfiledPIDController;
@@ -13,12 +15,15 @@ import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.units.measure.LinearVelocity;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.PoseEstimation;
 import frc.robot.data.ChassisSpeedsFilter;
 
 import java.util.function.Supplier;
@@ -31,11 +36,9 @@ public class Swerve extends SubsystemBase {
 	private final PIDController yController;
 	private final ProfiledPIDController thetaController;
 	private final HolonomicDriveController driveController;
-	private final SwerveDrivePoseEstimator poseEstimator;
 	private final ChassisSpeedsFilter filter;
 
 	private Pose2d targetPose = Pose2d.kZero;
-	private Rotation2d lockedTargetPose = Rotation2d.kZero;
 
 	public Swerve(SwerveIO io) {
 		this.io = io;
@@ -46,15 +49,17 @@ public class Swerve extends SubsystemBase {
 				new TrapezoidProfile.Constraints(TurnConstants.MAX_TURN_SPEED.in(RadiansPerSecond),
 						TurnConstants.MAX_TURN_ACCELERATION.in(RadiansPerSecondPerSecond)));
 		driveController = new HolonomicDriveController(xController, yController, thetaController);
-
-		poseEstimator = new SwerveDrivePoseEstimator(DriveConstants.KINEMATICS, Rotation2d.kZero,
-				io.getModulePositions(), Pose2d.kZero);
 		filter = new ChassisSpeedsFilter(DriveConstants.MAX_DRIVE_ACCELERATION, TurnConstants.MAX_TURN_ACCELERATION);
 	}
 
-	@Override
-	public void periodic() {
-		poseEstimator.update(io.getHeading(), io.getModulePositions());
+	@NotLogged
+	public Rotation2d getHeading() {
+		return io.getHeading();
+	}
+
+	@NotLogged
+	public SwerveModulePosition[] getModulePositions() {
+		return io.getModulePositions();
 	}
 
 	public Command o_zeroGyro() {
@@ -66,20 +71,20 @@ public class Swerve extends SubsystemBase {
 				.driveSpeeds(ChassisSpeeds.fromFieldRelativeSpeeds(vX.get(), vY.get(), vTheta.get(), io.getHeading())));
 	}
 
-	public Command l_driveToPose(Pose2d relativePose) {
+	public Command l_driveToPose(Pose2d targetRelativePose, Supplier<Pose2d> currentPose) {
 		return startRun(() -> {
-			targetPose = relativePose;
+			targetPose = targetRelativePose;
 			filter.reset(io.getHeading());
-		}, () -> io.driveSpeeds(filter.calculate(driveController.calculate(poseEstimator.getEstimatedPosition(),
+		}, () -> io.driveSpeeds(filter.calculate(driveController.calculate(currentPose.get(),
 				targetPose, 0, targetPose.getRotation())))).until(driveController::atReference);
 	}
 
-	public Command f_driveLocked(Supplier<LinearVelocity> vX, Supplier<LinearVelocity> vY, Supplier<Pose2d> lockedPose) {
+	public Command f_driveLocked(Supplier<LinearVelocity> vX, Supplier<LinearVelocity> vY, Supplier<Angle> lockedAngle) {
 		return startRun(() -> thetaController.reset(io.getHeading().getRadians()),
 				() -> io.driveSpeeds(
 						ChassisSpeeds.fromFieldRelativeSpeeds(vX.get(), vY.get(),
 								RadiansPerSecond.of(thetaController.calculate(io.getHeading().getRadians(),
-										Math.atan2(lockedPose.get().getY(), lockedPose.get().getX()))),
-								io.getHeading())));
+										lockedAngle.get().in(Radians))), io.getHeading())
+        ));
 	}
 }
