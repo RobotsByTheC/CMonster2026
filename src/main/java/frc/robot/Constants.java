1e14a22806434c37f17d47e8de60bb88bb3f5cd5
Peter <85247098+PeterLangendorf@users.noreply.github.com>
2026-02-05 16:05:44 -0500

    Add CAN ids for the left and right feeder sparks

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -72,7 +72,9 @@ public class Constants {
 		public static final int FLYWHEEL_LEFT_B_CAN_ID = 12;
 		public static final int FLYWHEEL_RIGHT_A_CAN_ID = 13;
 		public static final int FLYWHEEL_RIGHT_B_CAN_ID = 14;
-		public static final int HOOD_CAN_ID = 16;
+    public static final int FEEDER_LEFT_CAN_ID = 15;
+    public static final int FEEDER_RIGHT_CAN_ID = 16;
+    public static final int HOOD_CAN_ID = 17;
 
     public static final int LEFT_CNC_BOTTOM = 21;
     public static final int LEFT_CNC_MIDDLE = 22;
