faff85ce132b32e53f573daf123519784bfcea13
Peter <85247098+PeterLangendorf@users.noreply.github.com>
2026-02-13 20:06:04 -0500

    Falsify canandcolor values

--- src/main/java/frc/robot/subsystems/shooter/feeder/RealFeederIO.java
+++ src/main/java/frc/robot/subsystems/shooter/feeder/RealFeederIO.java
@@ -2,7 +2,6 @@ package frc.robot.subsystems.shooter.feeder;
 
 import static edu.wpi.first.units.Units.Amps;
 
-import com.reduxrobotics.sensors.canandcolor.Canandcolor;
 import com.revrobotics.PersistMode;
 import com.revrobotics.ResetMode;
 import com.revrobotics.spark.SparkMax;
@@ -18,18 +17,18 @@ import edu.wpi.first.units.measure.Voltage;
 public class RealFeederIO implements FeederIO {
   private final SparkMax spark;
 
-  private final Canandcolor bottom;
-  private final Canandcolor middle;
-  private final Canandcolor top;
+  // private final Canandcolor bottom;
+  // private final Canandcolor middle;
+  // private final Canandcolor top;
 
   public RealFeederIO(boolean inverted, int sparkCAN, int bottomCAN, int middleCAN, int topCAN) {
     spark = new SparkMax(sparkCAN, SparkLowLevel.MotorType.kBrushless);
     SparkBaseConfig config = new SparkMaxConfig().inverted(inverted).idleMode(SparkBaseConfig.IdleMode.kBrake);
     spark.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
 
-    bottom = new Canandcolor(bottomCAN);
-    middle = new Canandcolor(middleCAN);
-    top = new Canandcolor(topCAN);
+    // bottom = new Canandcolor(bottomCAN);
+    // middle = new Canandcolor(middleCAN);
+    // top = new Canandcolor(topCAN);
   }
 
   @Override
@@ -49,11 +48,11 @@ public class RealFeederIO implements FeederIO {
 
   @Override
   public boolean isBallAtFlywheel() {
-    return top.getProximity() < 0.1;
+    return true;
   }
 
   @Override
   public boolean isBallReadyToFire() {
-    return bottom.getProximity() < 0.1;
+    return true;
   }
 }
