0726e2f0cffd6dcac707d64f35becb4cfc3b4612
Peter <85247098+PeterLangendorf@users.noreply.github.com>
2026-02-13 20:10:39 -0500

    Add software trigger to stop

--- src/main/java/frc/robot/subsystems/shooter/hood/RealHoodIO.java
+++ src/main/java/frc/robot/subsystems/shooter/hood/RealHoodIO.java
@@ -19,6 +19,7 @@ public class RealHoodIO implements HoodIO {
   private final SparkMax spark;
   private final RelativeEncoder encoder;
   private final SparkLimitSwitch limitSwitch;
+  private final Trigger isPressed;
 
   public RealHoodIO() {
     spark = new SparkMax(HOOD_CAN_ID, SparkLowLevel.MotorType.kBrushless);
@@ -30,6 +31,10 @@ public class RealHoodIO implements HoodIO {
 
     encoder = spark.getEncoder();
     limitSwitch = spark.getForwardLimitSwitch();
+
+    isPressed = new Trigger(limitSwitch::isPressed);
+
+    isPressed.onTrue(Commands.runOnce(() -> encoder.setPosition(0)));
   }
 
   @Override
