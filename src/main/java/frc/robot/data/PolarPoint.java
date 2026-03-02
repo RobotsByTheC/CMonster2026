0c72dc04530f3c5160a234af99134cbcd00b37f3
Peter <85247098+PeterLangendorf@users.noreply.github.com>
2026-03-02 15:05:19 -0500

    Add funzies method to polarpoint class

--- src/main/java/frc/robot/data/PolarPoint.java
+++ src/main/java/frc/robot/data/PolarPoint.java
@@ -1,7 +1,15 @@
 package frc.robot.data;
 
+import static edu.wpi.first.units.Units.Radians;
+
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.Distance;
 
 public record PolarPoint(Distance distance, Angle angle) {
+  public Distance getX() {
+    return distance.times(Math.cos(angle.in(Radians)));
+  }
+  public Distance getY() {
+    return distance.times(Math.sin(angle.in(Radians)));
+  }
 }
