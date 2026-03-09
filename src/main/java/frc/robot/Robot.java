5657f7f2cd5d2c0a5fbebb26345c4c0b72ca6cef
Peter <85247098+PeterLangendorf@users.noreply.github.com>
2026-03-09 17:01:12 -0400

    Remove subsystems

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -50,11 +50,11 @@ import frc.robot.subsystems.swerve.Swerve;
 @Logged
 public class Robot extends TimedRobot {
   private Command autonomousCommand;
-  private final Intake intake;
+//  private final Intake intake;
   private final Swerve swerve;
   private final Shooter shooter;
   // private final PoseEstimation poseEstimation;
-  private final Hopper hopper;
+//  private final Hopper hopper;
 
   public MutDistance shooterSimDistance = Meters.mutable(1);
   public MutVoltage appliedVoltage = Volts.mutable(0);
@@ -65,15 +65,15 @@ public class Robot extends TimedRobot {
 
   public Robot() {
     if (Robot.isSimulation()) {
-      intake = new Intake(new SimIntakeIO());
+//      intake = new Intake(new SimIntakeIO());
       swerve = new Swerve(new SimSwerveIO());
       shooter = new Shooter(false);
-      hopper = new Hopper(new SimHopperIO());
+//      hopper = new Hopper(new SimHopperIO());
     } else {
-      intake = new Intake(new RealIntakeIO());
+//      intake = new Intake(new RealIntakeIO());
       swerve = new Swerve(new RealSwerveIO());
       shooter = new Shooter(true);
-      hopper = new Hopper(new RealHopperIO());
+//      hopper = new Hopper(new RealHopperIO());
     }
 
     // poseEstimation = new PoseEstimation();
@@ -90,9 +90,9 @@ public class Robot extends TimedRobot {
     Epilogue.configure(config -> config.backend = EpilogueBackend.multi(new FileBackend(DataLogManager.getLog()),
         new NTEpilogueBackend(NetworkTableInstance.getDefault())));
 
-    intake.setDefaultCommand(intake.f_stowAndIdle());
+//    intake.setDefaultCommand(intake.f_stowAndIdle());
     swerve.setDefaultCommand(f_driveWithFlightSticks());
-    hopper.setDefaultCommand(hopper.f_idle());
+//    hopper.setDefaultCommand(hopper.f_idle());
 
     bindDriverButtons();
     bindOperatorButtons();
