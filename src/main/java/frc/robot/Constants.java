2dd08e72017c8984048c0aac2bd542855d7b49b4
Peter <85247098+PeterLangendorf@users.noreply.github.com>
2026-03-09 20:12:38 -0400

    Increase driving speed

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -30,7 +30,7 @@ public class Constants {
 
   public static class SwerveConstants {
     public static class DriveConstants {
-      public static final LinearVelocity MAX_DRIVE_SPEED = MetersPerSecond.of(2);
+      public static final LinearVelocity MAX_DRIVE_SPEED = MetersPerSecond.of(4.5);
       public static final LinearAcceleration MAX_DRIVE_ACCELERATION = MetersPerSecondPerSecond.of(2);
       public static final Distance WHEEL_DIAMETER = Inches.of(3);
       public static final double DRIVE_MOTOR_REDUCTION = 4.71428571;
