bd2637a75433b2d250ab956782426e0f916e7497
Peter <85247098+PeterLangendorf@users.noreply.github.com>
2026-03-11 17:08:25 -0400

    Remove old comment

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -17,8 +17,6 @@ import edu.wpi.first.units.measure.Voltage;
 import static edu.wpi.first.units.Units.*;
 
 public class Constants {
-  // CHANGE THESE, NOTHING ELSE
-
   public static class SwerveConstants {
     public static class DriveConstants {
       public static final LinearVelocity MAX_DRIVE_SPEED = MetersPerSecond.of(4.5);
