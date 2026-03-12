f635f33a131dcbc16a0a496259b021ad0fde9d39
Peter <85247098+PeterLangendorf@users.noreply.github.com>
2026-03-11 20:49:23 -0400

    Change lookup table to more realistic values

--- src/main/java/frc/robot/data/LookupTable.java
+++ src/main/java/frc/robot/data/LookupTable.java
@@ -62,16 +62,11 @@ public class LookupTable {
   }
 
   static {
-    fill(Meters.of(1), RPM.of(2000), Degrees.of(5));
-    fill(Meters.of(1.1), RPM.of(2100), Degrees.of(7.5));
-    fill(Meters.of(1.2), RPM.of(2200), Degrees.of(10));
-    fill(Meters.of(1.3), RPM.of(2300), Degrees.of(12.5));
-    fill(Meters.of(1.4), RPM.of(2400), Degrees.of(15));
-    fill(Meters.of(1.5), RPM.of(2500), Degrees.of(17.5));
-    fill(Meters.of(1.6), RPM.of(2600), Degrees.of(20));
-    fill(Meters.of(1.7), RPM.of(2700), Degrees.of(22.5));
-    fill(Meters.of(1.8), RPM.of(2800), Degrees.of(25));
-    fill(Meters.of(1.9), RPM.of(2900), Degrees.of(27.5));
-    fill(Meters.of(2), RPM.of(3000), Degrees.of(30));
+    fill(Meters.of(0), RPM.of(0), Degrees.of(0));
+    fill(Meters.of(1), RPM.of(500), Degrees.of(2.5));
+    fill(Meters.of(2), RPM.of(1000), Degrees.of(5));
+    fill(Meters.of(3), RPM.of(1500), Degrees.of(7.5));
+    fill(Meters.of(4), RPM.of(2000), Degrees.of(10));
+    fill(Meters.of(5), RPM.of(2500), Degrees.of(12.5));
   }
 }
