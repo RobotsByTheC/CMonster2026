983d7bf5b685345986444ce4d02fb34e06ddc208
Peter <85247098+PeterLangendorf@users.noreply.github.com>
2026-03-20 20:26:45 -0400

    Made default command RSL blink

--- src/main/java/frc/robot/subsystems/leds/LEDs.java
+++ src/main/java/frc/robot/subsystems/leds/LEDs.java
@@ -7,6 +7,7 @@ import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.AddressableLEDBufferView;
 import edu.wpi.first.wpilibj.LEDPattern;
+import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -15,21 +16,21 @@ public class LEDs extends SubsystemBase {
 
   private final AddressableLED leds;
   private final AddressableLEDBuffer ledBuffer;
-  private AddressableLEDBufferView frontSection;
-  private AddressableLEDBufferView backSection;
+  private final AddressableLEDBufferView frontSection;
+  private final AddressableLEDBufferView backSection;
 
   public LEDs() {
     leds = new AddressableLED(9);
 
     ledBuffer = new AddressableLEDBuffer(57);
-    this.frontSection = ledBuffer.createView(0, 33);
-    this.backSection = ledBuffer.createView(34, 56);
+    this.frontSection = ledBuffer.createView(0, 33); // 34 LEDs on the front
+    this.backSection = ledBuffer.createView(34, 56); // 24 LEDs on the back
     leds.setLength(ledBuffer.getLength());
 
-    leds.setData(ledBuffer);
     leds.start();
-
+    leds.setData(ledBuffer);
   }
+
   public Command runPattern(LEDPattern pattern) {
     LEDPattern pattern_dim = pattern.atBrightness((Percent.of(75)));
     return run(() -> {
@@ -37,7 +38,11 @@ public class LEDs extends SubsystemBase {
       pattern_dim.applyTo(backSection);
       leds.setData(ledBuffer); // send pixel data to the LED strips
     }).ignoringDisable(true);
+  }
 
+  public Command rslBlink() {
+    return runPattern(LEDPattern.solid(Color.kOrangeRed)
+        .synchronizedBlink(RobotController::getRSLState));
   }
 
   public Command showFlywheelAtSpeed() {
