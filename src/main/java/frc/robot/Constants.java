c7014ad2596fd1c281ff277dac7d08ed1350d8aa
Peter <85247098+PeterLangendorf@users.noreply.github.com>
2026-03-23 20:24:49 -0400

    Increase hopper outtake voltage

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -153,7 +153,7 @@ public class Constants {
 
   public static class HopperConstants {
     public static final Voltage INTAKE_VOLTAGE = Volts.of(7);
-    public static final Voltage OUTTAKE_VOLTAGE = Volts.of(-5);
+    public static final Voltage OUTTAKE_VOLTAGE = Volts.of(-7);
   }
 
   public static class VisionConstants {
